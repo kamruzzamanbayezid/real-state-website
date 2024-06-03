2b19447c86edac2b46fa8ac5fb8b3a1ab6e1256b
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-06-03 23:14:29 +0600

    added basic project setup with react router dom

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayouts from "../Layouts/MainLayouts";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+      {
+            path: '/',
+            element: <MainLayouts />,
+            children: [
+                  {
+                        path: '/',
+                        element: <Home />
+                  }
+            ],
+      }
+]);
+
+export default router;
