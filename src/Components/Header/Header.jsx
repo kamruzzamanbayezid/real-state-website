a28d8350a0de2f0fc3fbc9e31b3ba22b2094fc56
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-06-04 11:12:36 +0600

    added navbar section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { FaArrowRightFromBracket } from "react-icons/fa6";
 
 const Header = () => {
       return (
-            <div className="set-width flex items-center justify-between pt-7 pb-4 border-b">
+            <div className="set-width md:flex items-center justify-between pt-7 pb-4 border-b hidden md:px-4 lg:px-0">
                   <div className="flex items-center gap-6">
                         <span className="flex items-center gap-2"><FaPhone className="text-[#BC986B] text-xl" /> <span className="text-base hover:text-[#BC986B]">01882888860</span></span>
                         <span className="flex items-center gap-2"><CiMail className="text-[#BC986B] text-2xl" /> <span className="text-base hover:text-[#BC986B]">kmbayezid955@gmail.com</span></span>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+      const navLinks = <div className="flex flex-col lg:flex-row lg:items-center gap-3 lg:gap-6">
+            <li>
+                  <NavLink
+                        to='/'
+                        className={({ isActive }) => isActive ? 'common-color text-xl font-medium' : 'text-xl font-medium'}
+                  >Home</NavLink>
+            </li>
+            <li>
+                  <NavLink
+                        to='/updateProfile'
+                        className={({ isActive }) => isActive ? 'common-color text-xl font-medium' : 'text-xl font-medium'}
+                  >Update Profile</NavLink>
+            </li>
+            <li>
+                  <NavLink
+                        to='/userProfile'
+                        className={({ isActive }) => isActive ? 'common-color text-xl font-medium' : 'text-xl font-medium'}
+                  >User Profile</NavLink>
+            </li>
+      </div>
+
       return (
             <div className="set-width pt-4">
                   <div className="navbar bg-base-100">
@@ -8,37 +31,21 @@ const Navbar = () => {
                                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                                     </div>
-                                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                          <li><a>Item 1</a></li>
-                                          <li>
-                                                <a>Parent</a>
-                                                <ul className="p-2">
-                                                      <li><a>Submenu 1</a></li>
-                                                      <li><a>Submenu 2</a></li>
-                                                </ul>
-                                          </li>
-                                          <li><a>Item 3</a></li>
+                                    <ul tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                          {navLinks}
                                     </ul>
                               </div>
-                              <a className="btn btn-ghost text-xl">daisyUI</a>
+                              <Link to='/' className="font-bold text-2xl md:text-3xl">LegacyEstates</Link>
                         </div>
                         <div className="navbar-center hidden lg:flex">
-                              <ul className="menu menu-horizontal px-1">
-                                    <li><a>Item 1</a></li>
-                                    <li>
-                                          <details>
-                                                <summary>Parent</summary>
-                                                <ul className="p-2">
-                                                      <li><a>Submenu 1</a></li>
-                                                      <li><a>Submenu 2</a></li>
-                                                </ul>
-                                          </details>
-                                    </li>
-                                    <li><a>Item 3</a></li>
+                              <ul className="px-1">
+                                    {navLinks}
                               </ul>
                         </div>
                         <div className="navbar-end">
-                              <a className="btn">Button</a>
+                              <Link to='/login'>
+                                    <button className="text-white bg-[#BC986B] px-8 py-3 text-base font-medium">Login</button>
+                              </Link>
                         </div>
                   </div>
             </div>
