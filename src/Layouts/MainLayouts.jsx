2a64d90b6f73250efbfa9baf171e82350288fe7f
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-06-04 10:22:14 +0600

    added header section

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Header from "../Components/Header/Header";
 
 const MainLayouts = () => {
       return (
             <div>
+                  <Header />
+                  <Navbar />
                   <Outlet />
             </div>
       );
